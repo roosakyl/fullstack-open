a5fed7ce85d68e708b6423910040b61384f23b9d
Roosa Kyllönen <roosa.kyllonen@gmail.com>
2024-11-27 18:19:55 +0200

    Tehtävä 2.8 valmiiksi

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -5,21 +5,28 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const PersonInfo = ({name}) => {
+  const PersonInfo = ({name, number}) => {
     return (
-      <li key={name}>{name}</li>
+      <li key={name}>{name}: {number}</li>
     )
   }
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  
+
   const addName = (event) => {
     event.preventDefault();
-    const newPerson = {name: newName}
+    const newPerson = {name: newName, number: newNumber}
     const alreadyAdded = persons.filter((person) => person.name === newName);
     //If the name is already on the list, prevent addition and give an alert. If not, add the name to the list
     alreadyAdded.length > 0 ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(newPerson))
@@ -30,7 +37,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input onChange={handleInputChange} />
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +53,7 @@ const App = () => {
         <ul>
           {persons.map((person) => {
           return (
-            <PersonInfo name={person.name} key={person.name} />
+            <PersonInfo name={person.name} number={person.number} key={person.name} />
           )
         })}
         </ul>
